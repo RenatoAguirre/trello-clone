cbe2f8086b2a586579ab4465f36ff4336552bcc5
RenatoAguirre <raaguirre1@miuandes.cl>
2024-06-09 15:36:52 -0400

    feat: new board controllers for sending data to the views

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,11 @@
 class BoardsController < ApplicationController
     before_action :authenticate_user!, except: [:index]
     def index
-      @boards = Board.all
+      if !user_signed_in?
+        @boards = Board.all
+      else
+        @team_board_dict = get_team_board_dict_from_user(current_user.id)
+      end
     end
   
     def show
@@ -55,4 +59,34 @@ class BoardsController < ApplicationController
       State.new(name: "In Progress", board_id: @board.id).save
       State.new(name: "Done", board_id: @board.id).save
     end
-  end
+
+    def get_available_boards #returns an array of boards
+      teams = TeamMember.where(user_id: current_user.id)
+      boards = []
+      teams.each do |team|
+        boards.concat(Board.where(team_id: team.team_id))
+      end
+      return boards
+    end
+
+    def get_team_ids_from_user(c_user_id) 
+      #returns an array of team ids
+      team_ids = TeamMember.where(user_id: c_user_id).map { |team_member| team_member.team_id}
+      return team_ids
+    end
+
+    def get_team_names_from_user(user_id) 
+      #returns an array of team names
+      team_ids = get_team_ids_from_user(user_id)
+      team_names = Team.where(id: team_ids).map { |team| team.name}
+      return team_names
+    end
+    def get_team_board_dict_from_user(user_id) 
+      #returns a  dictionary of keys: team_name and values: list(boards)
+      team_ids = get_team_ids_from_user(user_id)
+      team_names = Team.where(id: team_ids).map { |team| [team.id, team.name]}.to_h
+      team_board_dicts = team_ids.map { |team_id| [team_names[team_id], Board.where(team_id: team_id)]}.to_h
+      return team_board_dicts
+    end
+    
+  end 
